21040e775418de43158d8a39b8d07dbad25a4e03
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2024-11-03 00:51:35 +0900

    create bottom tab

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:zenn_app/widgets/article_search.dart';
+import 'package:zenn_app/widgets/home.dart';
 
 void main() {
-  runApp(const ArticleSearch());
+  // runApp(const ArticleSearch());
+
+  runApp(const Home());
 }
 
---
+++ lib/widgets/home.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:zenn_app/widgets/article_search.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  static const _screens = [
+    ArticleSearch(),
+    ArticleSearch(),
+  ];
+
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: _screens[_selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          items: const <BottomNavigationBarItem> [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'ホーム'),
+            BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'お気に入り'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.notifications), label: 'お知らせ'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'アカウント'),
+          ],
+          type: BottomNavigationBarType.fixed,
+        ),
+      ),
+    );
+  }
+}
