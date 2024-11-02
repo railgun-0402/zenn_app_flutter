d0e472ef2ae99ca646c0e7790f9577331a813b6f
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2024-11-03 01:01:45 +0900

    repair lint check

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:zenn_app/widgets/article_search.dart';
+// import 'package:zenn_app/widgets/article_search.dart';
 import 'package:zenn_app/widgets/home.dart';
 
 void main() {
