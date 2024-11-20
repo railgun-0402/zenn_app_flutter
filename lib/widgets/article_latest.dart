0f1ec82b1b97b1dce2d125f599f847f994f7b3f8
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2024-11-20 23:52:29 +0900

    unused_import del

--- lib/widgets/article_latest.dart
+++ lib/widgets/article_latest.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:zenn_app/helper/storage_helper.dart';
 import 'package:zenn_app/models/article.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
